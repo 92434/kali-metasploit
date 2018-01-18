5f25c135b173657afd5d649971fbc2b80c4fdca6
92434 <31708893+92434@users.noreply.github.com>
2018-01-18 22:14:17 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,4 +3,5 @@
 # /etc/init.d/postgresql start
 echo deb http://http.kali.org/kali kali-rolling main contrib non-free >> /etc/apt/sources.list
 /usr/share/metasploit-framework/msfupdate
+/etc/init.d/postgresql start
 /usr/share/metasploit-framework/msfconsole			# Throw us into the Metasploit console
